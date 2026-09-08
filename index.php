8dcb057aa6eb3a1bc10a4bfb027d093566416b79
quinten802 <d317484@edu.curio.nl>
2026-09-08 11:47:29 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <img src="img/curio-03-geel-logo-rgb.png" alt="curio-logo">
+        <h1>Open dag</h1>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
