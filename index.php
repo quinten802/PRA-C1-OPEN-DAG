46397a99c5e110d62b70e09cfe005258268777f8
quinten802 <d317484@edu.curio.nl>
2026-09-09 12:17:03 +0200

    Afbeeldingen toegevoegd

--- index.php
+++ index.php
@@ -16,31 +16,31 @@
             <h2>Wat is de opleiding</h2>
             <div class="text-segment">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/coding.jpg" alt="coding" width="500">
             </div>
             <h2>Wat kun je na deze opleiding doen</h2>
             <div class="text-segment">
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/67 low quality.jpg" alt="coding" width="1000">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
             </div>
             <h2>Overzicht hoe de opleiding loopt</h2>
             <div class="text-segment">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/Opleidingsoverzicht 26-27.jpg" alt="overzicht" width="500">
             </div>
             <h2>Vakken die je in het eestre jaar krijgt</h2>
             <div class="text-segment">
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/Rooster eerste leerjaar.png" alt="rooster" width="700">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
             </div>
             <h2>Zak/slaag regeling bij reguliere vakken</h2>
             <div class="text-segment">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/GettyImages-1148667096.jpg" alt="tets" width="500">
             </div>
             <h2>Werken in groepen</h2>
             <div class="text-segment">
-                <img src="img/cry.png" alt="tets" width="1000">
+                <img src="img/coding-group.jpg" alt="code-groep" width="1000">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
             </div>
     </main>
