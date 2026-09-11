aa32063ed35af153c8ca7d789df281c08b319ca8
quinten802 <d317484@edu.curio.nl>
2026-09-11 10:47:08 +0200

    Made site responsive and added images

--- index.php
+++ index.php
@@ -8,40 +8,40 @@
 </head>
 <body>
     <header>
-        <img class="header-things" src="img/curio-03-geel-logo-rgb.png" alt="curio-logo" width="10%">
-        <h1 class="header-things">Open dag</h1>
+        <img class="header-img" src="img/curio-03-geel-logo-rgb.png" alt="curio-logo">
+        <h1>Open dag</h1>
     </header>
     <main>
         <div class="wrapper">
             <h2>Wat is de opleiding</h2>
             <div class="text-segment">
                 <p>Bij de opleiding software developer leer je alles over programmeren. Je leert talen zoals HTML, CSS, PHP, C# en meer! </p>
-                <img src="img/coding.jpg" alt="coding" width="500">
+                <img src="img/software-dev.jpg" alt="software-dev">
             </div>
             <h2>Wat kun je na deze opleiding doen</h2>
             <div class="text-segment">
-                <img src="img/67 low quality.jpg" alt="coding" width="1000">
+                <img src="img/coding.jpg" alt="coding">
                 <p>Je kan werken bij bedrijven die alleen software maken, je kan bij bedrijven werken die andere dingen hebben maar er ook geprogrammeerd wordt. Je kan ook een andere opleiding doen zoals game development doen en die kant op gaan.</p>
             </div>
             <h2>Overzicht hoe de opleiding loopt</h2>
             <div class="text-segment">
                 <p>De opleiding is verdeeld in blokken, elk jaar heb je 2 blokken. Als je een blok zakt blijf je in dat blok zitten.</p>
-                <img src="img/Opleidingsoverzicht 26-27.jpg" alt="overzicht" width="500">
+                <img src="img/Opleidingsoverzicht 26-27.jpg" alt="overzicht">
             </div>
             <h2>Vakken die je in het eestre jaar krijgt</h2>
             <div class="text-segment">
-                <img src="img/Rooster eerste leerjaar.png" alt="rooster" width="700">
+                <img src="img/Rooster eerste leerjaar.png" alt="rooster">
                 <p>Je krijgt Native, Web, PRA, DIV, Nederlands, Engels en Rekenen.</p>
             </div>
             <h2>Zak/slaag regeling bij reguliere vakken</h2>
             <div class="text-segment">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
-                <img src="img/GettyImages-1148667096.jpg" alt="tets" width="500">
+                <img src="img/GettyImages-1148667096.jpg" alt="tets">
             </div>
             <h2>Werken in groepen</h2>
             <div class="text-segment">
-                <img src="img/coding-group.jpg" alt="code-groep" width="1000">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos iste facilis sit, voluptatibus perferendis, aperiam minima exercitationem earum ipsam blanditiis, architecto repellendus reprehenderit! Non aperiam, libero omnis aut cum repudiandae.</p>
+                <img src="img/coding-group.jpg" alt="code-groep">
+                <p>Software development gaat in groepen, </p>
             </div>
     </main>
 </body>
