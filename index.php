5adf5eb8b9230691abdf93fd869afff5af6b2512
quinten802 <d317484@edu.curio.nl>
2026-09-15 14:42:58 +0200

    fixed minor things

--- index.php
+++ index.php
@@ -32,46 +32,44 @@
                 <img src="img/software-dev.jpg" alt="software-dev">
             </div>
             
-            <div id="na" class="segment">
-                <img src="img/coding.jpg" alt="coding">
-
+            <div id="na" class="segment reverse">
                 <div class="text-segment">
                     <h2>Na de opleiding</h2>
                     <p>Je kan werken bij bedrijven die alleen software maken, je kan bij bedrijven werken die andere dingen hebben maar er ook geprogrammeerd wordt. Je kan ook een andere opleiding doen zoals game development doen en die kant op gaan.</p>
                 </div>
+                <img src="img/coding.jpg" alt="coding">
             </div>
 
             <div id="overzicht" class="segment">
                 <div class="text-segment">
                     <h2>Overzicht over de opleiding</h2>
-                    <p>De opleiding is verdeeld in blokken, elk jaar heb je 2 blokken. Als je een blok zakt blijf je in dat blok zitten.</p>
+                    <p>De opleiding is verdeeld in blokken, elk jaar heb je 2 blokken. Als je een blok zakt blijf je in dat blok zitten. Je gaat dus niet terug naar de eerste als je blok D in de tweede zakt. Je moet ook nog stage lopen. Dat doe je in de tweede en de vierde.</p>
                 </div>
                 <img src="img/Opleidingsoverzicht 26-27.jpg" alt="overzicht">
             </div>
 
-            <div id="rooster" class="segment">
-                <img src="img/Rooster eerste leerjaar.png" alt="rooster">
+            <div id="rooster" class="segment reverse">
                 <div class="text-segment">
                     <h2>Het eerste jaar</h2>
-                    <p>Je krijgt Native, Web, PRA, DIV, Nederlands, Engels en Rekenen.</p>
+                    <p>Je krijgt Native, wat is programma's maken die op je laptop draaien. Web, wat is websites maken. PRA, waarbij je samen in een groep een opdracht krijgt die je in een periode van een paar weken moet maken, dit is vergelijkbaar met hoe werk er later uit ziet. Je hebt ook nog gewoon de reguliere vakken zoals Nederlands, Engels en Rekenen.</p>
                 </div>
+                <img src="img/Rooster eerste leerjaar.png" alt="rooster"va>
             </div>
 
             <div id="regeling" class="segment">
                 <div class="text-segment">
                     <h2>Zak/slaag regeling</h2>
-                    <p>Je krijgt ook examens voor Nederlands, Engels en Rekenen. Je moet deze examens halen om je diploma te halen. Als je zakt kan je altijd 1 herkansing krijgen. Je kan ook nog een derke kans krijgen maar dat is alleen in bepaalde situaties. Daarna is het betalen.</p>
+                    <p>Je krijgt ook examens voor Nederlands, Engels en Rekenen. Je moet deze examens halen om je diploma te halen. Als je zakt kan je altijd 1 herkansing krijgen. Je kan ook nog een derde kans krijgen maar dat is alleen in bepaalde situaties. Daarna is het betalen.</p>
                 </div>
-                <img src="img/GettyImages-1148667096.jpg" alt="tets">
-
+                <img src="img/GettyImages-1148667096.jpg" alt="tets">  
             </div>
 
-            <div id="groep" class="segment">
-                <img src="img/coding-group.jpg" alt="code-groep">
+            <div id="groep" class="segment reverse">
                 <div class="text-segment">
                     <h2>Werken in groepen</h2>
                     <p>Software development gaat in groepen, dat is ook wat PRA is. Je krijgt een opdracht en dat moet je samen plannen en maken in een periode van een paar weken.</p>
                 </div>
+                <img src="img/coding-group.jpg" alt="code-groep">
             </div>
     </main>
     <footer>
