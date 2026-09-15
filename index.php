7eaef6ce3d8f82d6924f3d082670d8accf0b4251
quinten802 <d317484@edu.curio.nl>
2026-09-15 12:02:50 +0200

    navigatie toegevoegd, layout gefixed, logo gefixed

--- index.php
+++ index.php
@@ -8,19 +8,25 @@
 </head>
 <body>
     <header>
-        <img class="header-img" src="img/curio-03-geel-logo-rgb.png" alt="curio-logo">
-        <div class="scrollButtons">
-            <button id="opleidingScroll">test</button>
-
+        <div class="logo">
+            <img class="header-img" src="img/curio-03-geel-logo-rgb.png" alt="curio-logo" width="1000000000000000%">
+            <h1>Open dag</h1>
         </div>
-        <h1>Open dag</h1>
+        <div class="buttons">
+            <button id="opleidingScroll">OVER DE OPLEIDING</button>
+            <button id="naOpleidingScroll">NA DE OPLEIDING</button>
+            <button id="overzichtScroll">OVERZICHT OVER DE OPLEIDING</button>
+            <button id="vakkenScroll">HET EERSTE JAAR</button>
+            <button id="reguliereScroll">ZAK EN SLAAG REGELING</button>
+            <button id="groepenScroll">WERKEN IN GROEPEN</button>
+        </div>
     </header>
     <main>
         <div class="wrapper">
 
             <div class="segment">
                 <div class="text-segment">
-                    <h2>Wat is de opleiding</h2>
+                    <h2>Over de opleiding</h2>
                     <p>Bij de opleiding software developer leer je alles over programmeren. Je leert talen zoals HTML, CSS, PHP, C# en meer!</p>
                 </div>
                 <img src="img/software-dev.jpg" alt="software-dev">
@@ -30,14 +36,14 @@
                 <img src="img/coding.jpg" alt="coding">
 
                 <div class="text-segment">
-                    <h2>Wat kun je na deze opleiding doen</h2>
+                    <h2>Na de opleiding</h2>
                     <p>Je kan werken bij bedrijven die alleen software maken, je kan bij bedrijven werken die andere dingen hebben maar er ook geprogrammeerd wordt. Je kan ook een andere opleiding doen zoals game development doen en die kant op gaan.</p>
                 </div>
             </div>
 
             <div class="segment">
                 <div class="text-segment">
-                    <h2>Overzicht hoe de opleiding loopt</h2>
+                    <h2>Overzicht over de opleiding</h2>
                     <p>De opleiding is verdeeld in blokken, elk jaar heb je 2 blokken. Als je een blok zakt blijf je in dat blok zitten.</p>
                 </div>
                 <img src="img/Opleidingsoverzicht 26-27.jpg" alt="overzicht">
@@ -46,14 +52,14 @@
             <div class="segment">
                 <img src="img/Rooster eerste leerjaar.png" alt="rooster">
                 <div class="text-segment">
-                    <h2>Vakken die je in het eestre jaar krijgt</h2>
+                    <h2>Het eerste jaar</h2>
                     <p>Je krijgt Native, Web, PRA, DIV, Nederlands, Engels en Rekenen.</p>
                 </div>
             </div>
 
             <div class="segment">
                 <div class="text-segment">
-                    <h2>Zak/slaag regeling bij reguliere vakken</h2>
+                    <h2>Zak/slaag regeling</h2>
                     <p>Je krijgt ook examens voor Nederlands, Engels en Rekenen. Je moet deze examens halen om je diploma te halen. Als je zakt kan je altijd 1 herkansing krijgen. Je kan ook nog een derke kans krijgen maar dat is alleen in bepaalde situaties. Daarna is het betalen.</p>
                 </div>
                 <img src="img/GettyImages-1148667096.jpg" alt="tets">
@@ -73,6 +79,36 @@
 </body>
 <script>
 document.getElementById("opleidingScroll").onclick = function() {scrollFunction()};
+
+function scrollFunction() {
+  scrollTo(0, 1000);
+}
+
+document.getElementById("naOpleidingScroll").onclick = function() {scrollFunction()};
+
+function scrollFunction() {
+  scrollTo(0, 1000);
+}
+
+document.getElementById("overzichtScroll").onclick = function() {scrollFunction()};
+
+function scrollFunction() {
+  scrollTo(0, 1000);
+}
+
+document.getElementById("vakkenScroll").onclick = function() {scrollFunction()};
+
+function scrollFunction() {
+  scrollTo(0, 1000);
+}
+
+document.getElementById("reguliereScroll").onclick = function() {scrollFunction()};
+
+function scrollFunction() {
+  scrollTo(0, 1000);
+}
+
+document.getElementById("groepenScroll").onclick = function() {scrollFunction()};
 
 function scrollFunction() {
   scrollTo(0, 1000);
